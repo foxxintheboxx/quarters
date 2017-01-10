0da85ec748c4961d752a2bba49c73c593b48df69
Ian Fox <ianfox41@gmail.com>
2017-01-10 20:42:52 +0200

    Create FeedViewController

---
+++ Quarters/FeedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.swift
+//  Quarters
+//
+//  Created by Ian Fox on 1/9/17.
+//  Copyright © 2017 Quarters Inc. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+}
